979f12959a93e85aa9fbe2289edc351c27713e33
feather <feather@navercorp.com>
2022-04-19 17:34:42 +0900

    Add

---
+++ SayHelloLibCarthage/SayHelloLibCarthage.h
@@ -0,0 +1,18 @@
+//
+//  SayHelloLibCarthage.h
+//  SayHelloLibCarthage
+//
+//  Created by Feather on 2022/04/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SayHelloLibCarthage.
+FOUNDATION_EXPORT double SayHelloLibCarthageVersionNumber;
+
+//! Project version string for SayHelloLibCarthage.
+FOUNDATION_EXPORT const unsigned char SayHelloLibCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SayHelloLibCarthage/PublicHeader.h>
+
+
---
+++ SayHelloLibCarthage/Source/HelloMachine.swift
@@ -0,0 +1,18 @@
+//
+//  HelloMachine.swift
+//  SayHelloLibCarthage
+//
+//  Created by Feather on 2022/04/19.
+//
+
+import Foundation
+
+public class HelloMachine {
+    public init() {
+        
+    }
+    
+    public func say() {
+        print("Hello ~_~")
+    }
+}
